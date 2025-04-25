9fb0b67ac433c48dc5454547ffd56050f72e7711
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2025-04-25 20:20:25 +0930

    Add basic styles module to Header component
    - Header imports and uses Header.modules.css to apply locally scoped
    styles, ensuring a presentable user-interface for this component.

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import { useState } from "react";
 function App() {
   const pages = ["Home", "Shop"]; // Array containing the names of pages for navigation
   const [pageState, setPageState] = useState(pages[0]); // Current page state for conditional page rendering
+  const cart = []; // Will contain objects of each item the user has added to the cart
 
   return (<>
   {/* Render the Header component */}
   <Header 
     pages = {pages}
+    pageState = {pageState}
     setPageState = {setPageState}
+    cart = {cart}
   />
 
   {/* Conditional rendering of either the Home or Shop page depending on pageState */}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,29 @@
+import styles from '../css/Header.module.css';
+
 // Renders the Header component
-function Header({pages, setPageState}) {
-    return <>
-        <div className="header">
-            <p className="shop-name">ShopEase</p>
-            <section className="nav">
+function Header({ pages, pageState, setPageState, cart }) {
+    return (
+        <div className={styles.header}>
+            {/* Shop name */}
+            <p className={styles['shop-name']}>ShopEase</p>
+
+            {/* Nav section */}
+            <section className={styles.nav}> 
                 {/* Iterate through each page in the pages array and add its value to the navigation section */}
                 {pages.map((page) => (
-                    <p className="nav-link" key={page} onClick={() => setPageState(page)}>{page}</p>
+                    <p className={`${styles['nav-link']} ${page === pageState ? styles['active-nav'] : ''}`} // Conditionally assign active-nav class
+                        key={page} onClick={() => setPageState(page)}>
+                        {page}
+                    </p>
                 ))}
             </section>
+
+            {/* Shopping cart section */}
+            <div className={styles['cart-container']}>
+                <p>Cart</p>
+                <p>{cart.length}</p>
+            </div>
         </div>
-    </>
+    );
 }
 export default Header;
